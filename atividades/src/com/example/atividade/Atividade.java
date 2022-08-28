ebe27df4f5ba23c6e0be60ef2752e0713e4d1efd
kalistarz <110884795+kalistarz@users.noreply.github.com>
2022-08-28 02:27:42 -0300

    Add files via upload

---
+++ atividades/src/com/example/atividade/Atividade.java
@@ -0,0 +1,12 @@
+package com.example.atividade;
+import java.util.Scanner;
+
+public class Atividade {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int SOMA = A+B;
+        System.out.println("X = "+SOMA);
+    }
+}
